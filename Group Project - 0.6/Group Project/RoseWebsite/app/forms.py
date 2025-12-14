eb4552da9059b9aa4bca666dd97ffdf137120c91
Vio <jake6505@gmail.com>
2025-12-14 22:16:55 +0000

    Update 0.6
    Continued Development

---
+++ Group Project - 0.6/Group Project/RoseWebsite/app/forms.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, DateTimeField, DateField, IntegerField, TextAreaField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
+
+class PredictionForm(FlaskForm):
+    EventName = StringField('Event Name', validators=[DataRequired()])
+    EventDate = DateField('Date of Event')
+    EventLocation = StringField('Location of Event', validators=[DataRequired()])
+    EventBudget = IntegerField('Budget for Event (RM)', validators=[DataRequired()])
+    NumParticipents = IntegerField('Expected Number of Participents', validators=[DataRequired()])
+    EventDescription = TextAreaField('Event Description / Notes', validators=[DataRequired()])
+    submit = SubmitField('Run Prediction')
+
+class ImportForm(FlaskForm):
+    submit = SubmitField('Import Data')
---
+++ Group Project - 0.6/Group Project/RoseWebsite/app/routes.py
@@ -0,0 +1,38 @@
+from flask import render_template, flash, redirect, url_for
+from app import app
+from app.forms import LoginForm, PredictionForm, ImportForm
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Rose Admin 1275'}
+    return render_template('index.html', title='Home', user=user)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}'.format(
+            form.username.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/prediction', methods=['GET', 'POST'])
+def prediction():
+    form = PredictionForm()
+    if form.validate_on_submit():
+        flash("Prediction '{}' is generating".format(
+            form.EventName.data))
+        return redirect(url_for('revision'))
+    return render_template('prediction.html', title='Prediction', form=form)
+
+@app.route('/import', methods=['GET', 'POST'])
+def Import():
+    form = ImportForm()
+    if form.validate_on_submit():
+        return redirect(url_for('outcome'))
+    return render_template('import.html', title='Import', form=form)
+
+@app.route('/outcome')
+def outcome():
+    return render_template('outcome.html', title='Outcome')
