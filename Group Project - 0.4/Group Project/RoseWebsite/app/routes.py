c27ff55dacad268713198ee08c6ad6bd28dc66be
Vio <jake6505@gmail.com>
2025-12-14 22:14:20 +0000

    Update - 0.4
    Version Changes, all basic elements are there.
    Login form up to spec
    Content for Predictions and revision on the site
    A lot of general css added.
    added Icons to the nav bar
    Nav-bar changed to match designs

---
+++ Group Project - 0.4/Group Project/RoseWebsite/app/routes.py
@@ -0,0 +1,39 @@
+from flask import render_template, flash, redirect, url_for
+from app import app
+from app.forms import LoginForm, PredictionForm, RevisionForm
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Rose Admin 1275'}
+    return render_template('index.html', title='Home', user=user)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}'.format(
+            form.username.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/prediction', methods=['GET', 'POST'])
+def prediction():
+    form = PredictionForm()
+    if form.validate_on_submit():
+        flash("Prediction '{}' is generating".format(
+            form.EventName.data))
+        return redirect(url_for('revision'))
+    return render_template('prediction.html', title='Prediction', form=form)
+
+@app.route('/revision', methods=['GET', 'POST'])
+def revision():
+    form = RevisionForm()
+    if form.validate_on_submit():
+        flash("Revising Your Prediction")
+        return redirect(url_for('outcome'))
+    return render_template('revision.html', title='Revision', form=form)
+
+@app.route('/outcome')
+def outcome():
+    return render_template('outcome.html', title='Outcome')
